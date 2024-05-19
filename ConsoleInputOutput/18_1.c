fbb14e07ee2945b4025b8f3f565da820c15ede0a
kusuma0580 <85125082+kusuma0580@users.noreply.github.com>
2024-05-19 19:04:38 +0530

    Add files via upload

---
+++ ConsoleInputOutput/18_1.c
@@ -0,0 +1,10 @@
+/* To receive the string "We have got the guts, you get the glory!!" in 
+an array char str[ 100 ] which of the following functions would you 
+use? 
+1. scanf ( "%s", str ) ; 
+2. gets ( str ) ; 
+3. getchar ( str ) ; 
+4. fgetchar ( str ) ; */
+
+//2)gets(str)
+
---
+++ ConsoleInputOutput/18_3.c
@@ -0,0 +1,8 @@
+/*Which of the following can a format string of a printf( ) function 
+contain: 
+1. Characters, format specifications and escape sequences 
+2. Character, integers and floats 
+3. Strings, integers and escape sequences 
+4. Inverted commas, percentage sign and backslash character */
+
+//1)Characters, format specifications, and escape sequences.
